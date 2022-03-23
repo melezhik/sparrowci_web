c754ce6ba2c4e5da5bdaa2d4f8700abc0df6ba60
Alexey Melezhik <melezhik@gmail.com>
2022-03-23 14:07:16 +0000

    run script

---
+++ run.sh
@@ -0,0 +1,6 @@
+sparrowdo \
+--localhost \
+--with_sparky \
+--no_sudo \
+--desc="fastest - raku-Config-BINDish.git" \
+--tags=cpu=2,max=1,mem=5,project=https://github.com/vrurg/raku-Config-BINDish.git
