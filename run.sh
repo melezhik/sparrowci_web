f272f9e4a6e4b4566edd27550ad7b66f88332d7b
Alexey Melezhik <melezhik@gmail.com>
2022-03-27 02:57:27 +0000

    we need more memory

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sparrowdo \
 --with_sparky \
 --no_sudo \
 --desc="fastest - raku-Config-BINDish.git" \
---tags=cpu=2,max=1,mem=4,project=https://github.com/vrurg/raku-Config-BINDish.git
+--tags=cpu=2,max=1,mem=6,project=https://github.com/vrurg/raku-Config-BINDish.git
