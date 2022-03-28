88cf14ecea379b995694a94e808a7dbe5474d198
Alexey Melezhik <melezhik@gmail.com>
2022-03-28 21:18:07 +0000

    more fixes

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ sparrowdo \
 --localhost \
 --with_sparky \
 --no_sudo \
---desc="fastest - raku-Config-BINDish.git" \
+--desc="raku-Config-BINDish.git" \
 --tags=cpu=2,max=1,mem=6,project=Config-BINDish,scm=https://github.com/vrurg/raku-Config-BINDish.git
