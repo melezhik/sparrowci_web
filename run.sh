c37054a8c58af6e47323bb830ac60017d86a47ad
Alexey Melezhik <melezhik@gmail.com>
2022-04-06 19:14:02 +0000

    pass sha

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ sparrowdo \
 --localhost \
 --with_sparky \
 --no_sudo \
---desc="raku-Config-BINDish.git" \
---tags=cpu=2,mem=6,project=Config-BINDish,scm=https://github.com/vrurg/raku-Config-BINDish.git
+--desc="gh-melezhik-rakudist-teddy-bear" \
+--tags=cpu=2,mem=6,project=gh-melezhik-rakudist-teddy-bear,scm=https://github.com/melezhik/rakudist-teddy-bear.git,git-sha=cfbe4c7
