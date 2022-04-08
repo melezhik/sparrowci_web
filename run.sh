1f5a5b0a19574f1daf8ddbe9b67d1637544e5d1c
Alexey Melezhik <melezhik@gmail.com>
2022-04-08 15:55:34 +0000

    experimenting with mysql

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sparrowdo \
 --with_sparky \
 --no_sudo \
 --desc="gh-melezhik-rakudist-teddy-bear" \
---tags=cpu=2,mem=6,project=gh-melezhik-rakudist-teddy-bear,scm=https://github.com/melezhik/rakudist-teddy-bear.git,git-sha=cfbe4c7
+--tags=cpu=2,mem=6,project=gh-melezhik-rakudist-teddy-bear,scm=https://github.com/melezhik/rakudist-teddy-bear.git,git-sha=cfbe4c7,sparky-project=raku-with-mysql
