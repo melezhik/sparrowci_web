12889599719c463e5ad2acebd255fab8e22c4134
Alexey Melezhik <melezhik@gmail.com>
2022-07-03 12:16:09 +0300

    Update ci.sh

--- alpine/ci.sh
+++ alpine/ci.sh
@@ -13,7 +13,6 @@
   28 su --help
   34 su - builder -c 'abuild-keygen -a -n -i'
   35 nano /tmp/crt.package
-```sh
 mkdir -p ~/raku-packages/raku-kind
 cd raku-packages/raku-kind
 cat << 'HERE' > APKBUILD
@@ -54,48 +53,6 @@ HERE
 
 abuild checksum
 abuild -r
-/ # cat /tmp/crt.package 
-mkdir -p ~/raku-packages/raku-kind
-cd raku-packages/raku-kind
-cat << 'HERE' > APKBUILD
-# Contributor:
-# Maintainer:
-pkgname=raku-kind
-pkgver=0.2.2
-pkgrel=3
-pkgdesc="kind package"
-url="https://raku.land/cpan:KAIEPI/Kind"
-arch="all !s390x !riscv64"
-license="Artistic-2.0"
-depends="rakudo"
-makedepends="rakudo-dev"
-#checkdepends=""
-#install=""
-subpackages="$pkgname-doc"
-source="$pkgname-$pkgver.tar.gz::https://cpan.metacpan.org/authors/id//K/KA/KAIEPI/Perl6/Kind-0.2.2.tar.gz"
-builddir="$srcdir"/Kind-"$pkgver"
-
-
-check() {
-	# Replace with proper check command(s)
-	# prove -e 'raku -Ilib'
-	:
-}
-
-package() {
-	set -x	
-	ls -l	
-	RAKUDO_RERESOLVE_DEPENDENCIES=0 /usr/share/rakudo/tools/install-dist.raku \
-		--to="$pkgdir"/usr/share/rakudo/vendor --for=vendor
-	install -Dvm644 LICENSE META6.json README.md \
-		-t "$pkgdir"/usr/share/doc/"$pkgname"
-
-}
-HERE
-
-abuild checksum
-abuild -r
-```
 37 chmod a+x /tmp/crt.package
 38 su - builder -c /tmp/crt.package
 41 ls -l /home/builder/packages/raku-packages/x86_64/
